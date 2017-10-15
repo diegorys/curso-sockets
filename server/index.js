e1e8f70bd97473b146d13d4b7cdc8b2c211937dc
Diego de los Reyes <diegorys@gmail.com>
2017-10-15 23:27:14 +0200

    Finished

--- server/index.js
+++ server/index.js
@@ -9,10 +9,24 @@ app.get('/hola-mundo', function(req, res){
 	res.status(200).send('Hola mundo desde una ruta.');
 });
 
+var messages = [{
+	id: 1,
+	text: 'Bienvenido al chat privado de Socket.io y NodeJS de Diego de los Reyes...',
+	nickname: 'Bot - diegorys.es'
+}];
+
+
 io.on('connection', function(socket){
 	console.log('El cliente con IP ' + socket.handshake.address + " se ha conectado...");
+	socket.emit('messages', messages);
+	socket.on('add-message', function(data){
+		console.log("He recibido: ");
+		console.log(data);
+		messages.push(data);
+		io.sockets.emit('messages', messages);
+	});
 });
 
 server.listen(6677, function(){
-	console.log('Servidor está funcionando en http://localhost:6677');
+	console.log('Servidor está funcionando en http://localhost:6677');	
 });
